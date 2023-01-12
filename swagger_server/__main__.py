8c7f05f15207df752ad7f3e7fc6228da774a2a63
Nagendra Mishr <nagendra@microsoft.com>
2023-01-12 00:00:16 -0500

    added a response on /

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -9,8 +9,12 @@ def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Swagger DS/CS 519 Assessment - OpenAPI 3.0'}, pythonic_params=True)
-    app.run(port=8080)
 
+    @app.route('/')
+    def starting_url():
+        return "OK"
 
+    app.run(port=8000)
+    
 if __name__ == '__main__':
     main()
