20007b23a4eed69e1e1e3d753ccea09ad45af6d1
Nagendra Mishr <nagendra@microsoft.com>
2023-01-12 02:31:07 -0500

    try to get it working with gunicorn

---
+++ swagger_server/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import connexion
+
+from swagger_server import encoder
+
+app = connexion.App(__name__, specification_dir='./swagger/')
+
+app.app.json_encoder = encoder.JSONEncoder
+app.add_api('swagger.yaml', arguments={'title': 'Swagger DS/CS 519 Assessment - OpenAPI 3.0'}, pythonic_params=True)
+
+@app.route('/')
+def starting_url():
+    return "OK"
+
+if __name__ == '__main__':
+    app.run(port=8000)
--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -4,17 +4,15 @@ import connexion
 
 from swagger_server import encoder
 
+app = connexion.App(__name__, specification_dir='./swagger/')
 
-def main():
-    app = connexion.App(__name__, specification_dir='./swagger/')
-    app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('swagger.yaml', arguments={'title': 'Swagger DS/CS 519 Assessment - OpenAPI 3.0'}, pythonic_params=True)
+app.app.json_encoder = encoder.JSONEncoder
+app.add_api('swagger.yaml', arguments={'title': 'Swagger DS/CS 519 Assessment - OpenAPI 3.0'}, pythonic_params=True)
 
-    @app.route('/')
-    def starting_url():
-        return "OK"
+@app.route('/')
+def starting_url():
+    return "OK"
 
-    app.run(port=8000)
-    
+
 if __name__ == '__main__':
-    main()
+    app.run(port=8000)
