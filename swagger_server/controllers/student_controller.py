51aebe91cb64cceff8738d72ea9b423021522883
Nagendra Mishr <nagendra@microsoft.com>
2023-01-12 08:29:33 -0500

    fixed required parameters

--- swagger_server/controllers/student_controller.py
+++ swagger_server/controllers/student_controller.py
@@ -6,7 +6,7 @@ from swagger_server.models.student import Student  # noqa: E501
 from swagger_server import util
 
 
-def add_student(body, buid=None):  # noqa: E501
+def add_student(body, buid):  # noqa: E501
     """Enroll a new student to the university
 
     Adds a new student to the university # noqa: E501
@@ -23,7 +23,7 @@ def add_student(body, buid=None):  # noqa: E501
     return 'do some magic!'
 
 
-def add_student(university_id, name, semester_enrolled, status, buid=None):  # noqa: E501
+def add_student(university_id, name, semester_enrolled, status, buid):  # noqa: E501
     """Enroll a new student to the university
 
     Adds a new student to the university # noqa: E501
@@ -60,7 +60,7 @@ def delete_student(student_id, api_key=None):  # noqa: E501
     return 'do some magic!'
 
 
-def find_students_by_status(buid=None, status=None):  # noqa: E501
+def find_students_by_status(buid, status=None):  # noqa: E501
     """Finds Students by status
 
     Multiple status values can be provided with comma separated strings # noqa: E501
@@ -88,34 +88,34 @@ def get_student_by_id(student_id):  # noqa: E501
     return 'do some magic!'
 
 
-def list_grades(student_id, class_id, buid=None):  # noqa: E501
+def list_grades(buid, student_id, class_id):  # noqa: E501
     """Lists student grades in a class
 
     Lists student grades in a class # noqa: E501
 
+    :param buid: The caller&#x27;s BUID
+    :type buid: str
     :param student_id: ID of the student whose grades to return
     :type student_id: str
     :param class_id: 
     :type class_id: str
-    :param buid: The caller&#x27;s BUID
-    :type buid: str
 
     :rtype: List[Grade]
     """
     return 'do some magic!'
 
 
-def update_student(body, student_id, buid=None):  # noqa: E501
+def update_student(body, buid, student_id):  # noqa: E501
     """Update an existing student
 
     Update an existing student by Id # noqa: E501
 
     :param body: Update an existent student enrolled in the university
     :type body: dict | bytes
-    :param student_id: ID of student to update
-    :type student_id: str
     :param buid: The caller&#x27;s BUID
     :type buid: str
+    :param student_id: ID of student to update
+    :type student_id: str
 
     :rtype: Student
     """
@@ -124,7 +124,7 @@ def update_student(body, student_id, buid=None):  # noqa: E501
     return 'do some magic!'
 
 
-def update_student(university_id, name, semester_enrolled, status, student_id, buid=None):  # noqa: E501
+def update_student(university_id, name, semester_enrolled, status, buid, student_id):  # noqa: E501
     """Update an existing student
 
     Update an existing student by Id # noqa: E501
@@ -137,10 +137,10 @@ def update_student(university_id, name, semester_enrolled, status, student_id, b
     :type semester_enrolled: str
     :param status: 
     :type status: str
-    :param student_id: ID of student to update
-    :type student_id: str
     :param buid: The caller&#x27;s BUID
     :type buid: str
+    :param student_id: ID of student to update
+    :type student_id: str
 
     :rtype: Student
     """
